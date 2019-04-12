d22ed55b1095d37cd40059791c441fab88341187
Lauren Mateo <stlem04@moravian.edu>
2019-04-12 12:35:20 -0400

    edited doc_filter.py save path to work with Mac Mini

--- src/mirrulations_server/doc_filter.py
+++ src/mirrulations_server/doc_filter.py
@@ -10,8 +10,8 @@ import zipfile
 from mirrulations_core.mirrulations_logging import logger
 import mirrulations_core.documents_core as dc
 
-HOME_REGULATION_PATH = '/mnt/regulations-data/'
-CLIENT_LOG_PATH = '/mnt/regulations-data/client-logs/'
+HOME_REGULATION_PATH = '/desktop/regulations-data/'
+CLIENT_LOG_PATH = '/desktop/regulations-data/client-logs/'
 
 
 def process_doc(redis_server, json_data,
