4b8cbf9312cb89ae64c848cab057db376e13fb7b
edwharbison <edwharbison@gmail.com>
2019-04-28 13:04:56 -0400

    Fix up server tests a bit

--- tests/mirrulations_acceptance_tests/test_server.py
+++ tests/mirrulations_acceptance_tests/test_server.py
@@ -5,7 +5,7 @@ import json
 import os
 
 PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                    "../test_files/mirrulations_files/")
+                    '../test_files/mirrulations_files/')
 
 
 @pytest.fixture
@@ -25,41 +25,41 @@ def return_data_ids(number_ids):
 
 def test_root_gives_empty_json(client):
     result = client.get('/')
-    assert b'{}' == result.data
+    assert {} == json.loads(result.data)
 
 
 def test_when_one_job_in_db_then_job_returned_by_get_work(client):
     rm = RedisManager()
-    rm.add_to_queue(json.dumps({'job_id': "1234", 'type': "docs", 'data': ["Url1"], 'version': "0.5"}))
+    rm.add_to_queue(json.dumps({'job_id': '1234', 'type': 'docs', 'data': ['Url1'], 'version': '0.5'}))
 
     result = client.get('/get_work?client_id=asdf')
 
-    assert b'{"job_id": "1234", "type": "docs", "data": ["Url1"], "version": "0.5"}' == result.data
+    assert {'job_id': '1234', 'type': 'docs', 'data': ['Url1'], 'version': '0.5'} == json.loads(result.data)
     assert rm.does_job_exist_in_progress('1234')
 
 
 def test_when_two_jobs_in_db_returned_by_get_work(client):
     rm = RedisManager()
-    rm.add_to_queue(json.dumps({'job_id': "1234", 'type': "docs", 'data': ["Url1"], 'version': "0.5"}))
-    rm.add_to_queue(json.dumps({'job_id': "3456", 'type': "docs", 'data': ["Url2"], 'version': "0.5"}))
+    rm.add_to_queue(json.dumps({'job_id': '1234', 'type': 'docs', 'data': ['Url1'], 'version': '0.5'}))
+    rm.add_to_queue(json.dumps({'job_id': '3456', 'type': 'docs', 'data': ['Url2'], 'version': '0.5'}))
 
     result = client.get('/get_work?client_id=asdf')
     result_two = client.get('/get_work?client_id=asdf')
 
-    assert b'{"job_id": "1234", "type": "docs", "data": ["Url1"], "version": "0.5"}' == result.data
-    assert b'{"job_id": "3456", "type": "docs", "data": ["Url2"], "version": "0.5"}' == result_two.data
+    assert {'job_id': '1234', 'type': 'docs', 'data': ['Url1'], 'version': '0.5'} == json.loads(result.data)
+    assert {'job_id': '3456', 'type': 'docs', 'data': ['Url2'], 'version': '0.5'} == json.loads(result_two.data)
     assert rm.does_job_exist_in_progress('1234')
     assert rm.does_job_exist_in_progress('3456')
 
 
 def test_when_two_jobs_in_db_return_one_by_get_work(client):
     rm = RedisManager()
-    rm.add_to_queue(json.dumps({'job_id': "1234", 'type': "docs", 'data': ["Url1"], 'version': "0.5"}))
-    rm.add_to_queue(json.dumps({'job_id': "3456", 'type': "docs", 'data': ["Url2"], 'version': "0.5"}))
+    rm.add_to_queue(json.dumps({'job_id': '1234', 'type': 'docs', 'data': ['Url1'], 'version': '0.5'}))
+    rm.add_to_queue(json.dumps({'job_id': '3456', 'type': 'docs', 'data': ['Url2'], 'version': '0.5'}))
 
     result = client.get('/get_work?client_id=asdf')
 
-    assert b'{"job_id": "1234", "type": "docs", "data": ["Url1"], "version": "0.5"}' == result.data
+    assert {'job_id': '1234', 'type': 'docs', 'data': ['Url1'], 'version': '0.5'} == json.loads(result.data)
     assert rm.does_job_exist_in_progress('1234')
     assert rm.does_job_exist_in_queue('3456')
     assert rm.does_job_exist_in_progress('3456') is False
@@ -67,17 +67,20 @@ def test_when_two_jobs_in_db_return_one_by_get_work(client):
 
 def test_docs_job_in_db_return_doc_place_in_db_queue(client):
     rm = RedisManager()
-    rm.add_to_queue(b'{"data": ["Url1"], "version": "v0.5", "type": "docs", "job_id": "1234"}')
+    rm.add_to_queue(json.dumps({'data': ['Url1'], 'version': 'v0.5', 'type': 'docs', 'job_id': '1234'}))
 
     result = client.get('/get_work?client_id=asdf')
 
-    assert b'{"data": ["Url1"], "version": "v0.5", "type": "docs", "job_id": "1234"}' == result.data
+    assert {'data': ['Url1'], 'version': 'v0.5', 'type': 'docs', 'job_id': '1234'} == json.loads(result.data)
     assert rm.does_job_exist_in_progress('1234') is True
 
-    client.post("/return_docs", data={'file': open(PATH + "Archive.zip", 'rb'), 'json': json.dumps({'job_id': "1234", 'type': 'docs',
-                                                          "data": [[{"id": "AHRQ_FRDOC_0001-0036", "count": 1}]],
-                                                          'client_id': "abcd",
-                                                          "version": "0.5"})})
+    client.post('/return_docs',
+                data={'file': open(PATH + 'Archive.zip', 'rb'),
+                      'json': json.dumps({'job_id': '1234',
+                                          'type': 'docs',
+                                          'data': [[{'id': 'AHRQ_FRDOC_0001-0036', 'count': 1}]],
+                                          'client_id': 'abcd',
+                                          'version': '0.5'})})
     assert len(rm.get_all_items_in_queue()) == 1
     assert rm.does_job_exist_in_progress('1234') is False
 
@@ -85,15 +88,18 @@ def test_docs_job_in_db_return_doc_place_in_db_queue(client):
 def test_docs_job_return_multiple_doc_place_in_db_queue(client):
     rm = RedisManager()
 
-    rm.add_to_queue(b'{"data": ["Url1"], "version": "v0.5", "type": "docs", "job_id": "1234"}')
+    rm.add_to_queue(json.dumps({'data': ['Url1'], 'version': 'v0.5', 'type': 'docs', 'job_id': '1234'}))
     result = client.get('/get_work?client_id=asdf')
 
-    client.post("/return_docs",
-                data={'file': open(PATH + "Archive.zip", 'rb'),
-                      'json': json.dumps({'job_id': "1234", 'type': 'docs',
-                                          "data": [[{"id": "AHRQ_FRDOC_0001-0036", "count": 1}],
-                                                    [{"id": "AHRQ_FRDOC_0002-0036", "count": 2}]],
-                                          'client_id': "abcd", "version": "0.5"})})
+    assert {'data': ['Url1'], 'version': 'v0.5', 'type': 'docs', 'job_id': '1234'} == json.loads(result.data)
+    assert rm.does_job_exist_in_progress('1234') is True
+
+    client.post('/return_docs',
+                data={'file': open(PATH + 'Archive.zip', 'rb'),
+                      'json': json.dumps({'job_id': '1234', 'type': 'docs',
+                                          'data': [[{'id': 'AHRQ_FRDOC_0001-0036', 'count': 1}],
+                                                   [{'id': 'AHRQ_FRDOC_0002-0036', 'count': 2}]],
+                                          'client_id': 'abcd', 'version': '0.5'})})
     assert len(rm.get_all_items_in_queue()) == 2
     assert rm.does_job_exist_in_progress('1234') is False
 
@@ -103,14 +109,17 @@ def test_docs_job_return_multiple_doc_place_in_db_queue_with_files(client):
     with open(PATH + 'return_data.txt', 'r') as file:
         return_data = file.read().replace('\n', '')
 
-    rm.add_to_queue(b'{"data": ["Url1"], "version": "v0.5", "type": "docs", "job_id": "1234"}')
+    rm.add_to_queue(json.dumps({'data': ['Url1'], 'version': 'v0.5', 'type': 'docs', 'job_id': '1234'}))
     result = client.get('/get_work?client_id=asdf')
+    
+    assert {'data': ['Url1'], 'version': 'v0.5', 'type': 'docs', 'job_id': '1234'} == json.loads(result.data)
+    assert rm.does_job_exist_in_progress('1234') is True
 
-    client.post("/return_docs",
-                data={'file': open(PATH + "Archive.zip", 'rb'),
-                      'json': json.dumps({'job_id': "1234", 'type': 'docs',
-                                          "data": json.loads(return_data),
-                                          'client_id': "abcd", "version": "0.5"})})
+    client.post('/return_docs',
+                data={'file': open(PATH + 'Archive.zip', 'rb'),
+                      'json': json.dumps({'job_id': '1234', 'type': 'docs',
+                                          'data': json.loads(return_data),
+                                          'client_id': 'abcd', 'version': '0.5'})})
     assert len(rm.get_all_items_in_queue()) == 2
     assert rm.does_job_exist_in_progress('1234') is False
 
@@ -120,14 +129,18 @@ def test_docs_job_return_1000_doc_place_in_db_queue_with_helper_method(client):
 
     return_data = return_data_ids(1000)
 
-    rm.add_to_queue(b'{"data": ["Url1"], "version": "v0.5", "type": "docs", "job_id": "1234"}')
+    rm.add_to_queue(json.dumps({'data': ['Url1'], 'version': 'v0.5', 'type': 'docs', 'job_id': '1234'}))
     result = client.get('/get_work?client_id=asdf')
 
-    client.post("/return_docs",
-                data={'file': open(PATH + "Archive.zip", 'rb'),
-                      'json': json.dumps({'job_id': "1234", 'type': 'docs',
-                                          "data": json.loads(return_data),
-                                          'client_id': "abcd", "version": "0.5"})})
+    assert {'data': ['Url1'], 'version': 'v0.5', 'type': 'docs', 'job_id': '1234'} == json.loads(result.data)
+    assert rm.does_job_exist_in_progress('1234') is True
+
+    client.post('/return_docs',
+                data={'file': open(PATH + 'Archive.zip', 'rb'),
+                      'json': json.dumps({'job_id': '1234', 'type': 'docs',
+                                          'data': json.loads(return_data),
+                                          'client_id': 'abcd', 'version': '0.5'})})
+
     assert len(rm.get_all_items_in_queue()) == 1000
     assert rm.does_job_exist_in_progress('1234') is False
 
@@ -137,13 +150,17 @@ def test_docs_job_return_1000_doc_place_in_db_queue_with_helper_method_and_1000_
 
     return_data = return_data_ids(1000)
 
-    rm.add_to_queue(b'{"data": ["Url1"], "version": "v0.5", "type": "docs", "job_id": "1234"}')
+    rm.add_to_queue(json.dumps({'data': ['Url1'], 'version': 'v0.5', 'type': 'docs', 'job_id': '1234'}))
     result = client.get('/get_work?client_id=asdf')
+    
+    assert {'data': ['Url1'], 'version': 'v0.5', 'type': 'docs', 'job_id': '1234'} == json.loads(result.data)
+    assert rm.does_job_exist_in_progress('1234') is True
 
-    client.post("/return_docs",
-                data={'file': open(PATH + "Big_Archive.zip", 'rb'),
-                      'json': json.dumps({'job_id': "1234", 'type': 'docs',
-                                          "data": json.loads(return_data),
-                                          'client_id': "abcd", "version": "0.5"})})
+    client.post('/return_docs',
+                data={'file': open(PATH + 'Big_Archive.zip', 'rb'),
+                      'json': json.dumps({'job_id': '1234', 'type': 'docs',
+                                          'data': json.loads(return_data),
+                                          'client_id': 'abcd', 'version': '0.5'})})
+
     assert len(rm.get_all_items_in_queue()) == 1000
     assert rm.does_job_exist_in_progress('1234') is False
